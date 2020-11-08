510e429d39d7a963ae836a7a9c3f4c1791b897fa
QiusenHuangGT <qiusenhuang17@hotmail.com>
2020-11-07 22:47:26 -0500

    adding date and amount value check and adding more observability for error

--- View/ParserWrapper.py
+++ View/ParserWrapper.py
@@ -35,7 +35,10 @@ def read_from_file(filename, type):
     for i in data:
         customer_set.add(i[5])
 
+    rowCount = 1
     for i in data:
+        checkRowIntegrity(i, rowCount)
+        rowCount += 1
         if i[6] == '':
             i[6] = '无'
         date = i[1].split('/')
@@ -54,6 +57,19 @@ def read_from_file(filename, type):
         db_manager.execute(command)
     db_manager.commit()
     db_manager.close()
+
+def checkRowIntegrity(rawRow, rowNum):
+    # check if valid date
+    try:
+        format_date(rawRow[1])
+    except:
+        raise ValueError(f"{rowNum}行日期有误")
+
+    try:
+        float(rawRow[4])
+    except:
+        raise ValueError(f"{rowNum}行数量无法转换成数码")
+
 
 def format_date(date_str):
     temp = str(date_str).split('/')
@@ -77,7 +93,7 @@ def read_from_source(name):
                 if 'END' not in row:
                     if row[len(row)-1] != 'n':
                         c = len(row)-1
-                        raise ValueError(f"csv格式错误！可能的错误位置：{r}行{c}列")
+                        raise ValueError(f"csv格式错误！请保证数据行最后一列是 n ，文件最后一行有 END。可能的错误位置：{r}行{c}列 \n 内容{row}")
                     else:
                         data.append(row)
     return data
